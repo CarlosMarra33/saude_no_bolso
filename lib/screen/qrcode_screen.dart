2aac8238ecef81341b354704b601f4e78b471bd4
Carlos Marra <carlos.marra@al.infnet.edu.br>
2020-10-24 12:49:16 -0300

    qrCode

--- lib/screen/qrcode_screen.dart
+++ lib/screen/qrcode_screen.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import 'package:saude_no_bolso/common/custom_drawer/custom_drawer.dart';
 
 class QrCodeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    QrImage(
+      data: 'This is a simple QR code',
+      version: QrVersions.auto,
+      size: 320,
+      gapless: false,
+    );
     return Scaffold(
       extendBodyBehindAppBar: true,
       drawer: CustomDrawer(),
@@ -12,9 +19,28 @@ class QrCodeScreen extends StatelessWidget {
         elevation: 0,
         title: Text("QR Code"),
       ),
-      body: Container(
-        color: Colors.grey,
+      
+body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("Código gerado com o texto:",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            SizedBox(height: 16,),
+
+            QrImage(
+              data: '1234455',
+              gapless: true,
+              size: 250,
+              errorCorrectionLevel: QrErrorCorrectLevel.H,
+            )
+          ],
+        ),
       ),
-    );
+    );    
   }
 }
