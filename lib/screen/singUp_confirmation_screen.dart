4d9fced5a7c7f36d6ba6f4b0ab98456f5bdc03fd
Carlos Marra <carlos.marra@al.infnet.edu.br>
2020-10-24 22:46:03 -0300

    front_em andameto do singUp_confirmation_screen

--- lib/screen/singUp_confirmation_screen.dart
+++ lib/screen/singUp_confirmation_screen.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:saude_no_bolso/screen/profile_screen.dart';
 
 class SingUpConfirmation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      extendBodyBehindAppBar: false,
+      appBar: AppBar(
+        titleSpacing: MediaQuery.of(context).size.width * 2.8/10,
+        backgroundColor:Color.fromRGBO(26, 132, 116, 1),
+        elevation: 0,
+          
+      ),
+      body: Stack(
+        children: [
+          Container(
+        color: Color(0xFF1A8474),
+        child: Column(          
+          children: [ 
+            Image.network(''),
+            Text('CAFASCASCASCAS',
+            ),
+            Text('CAFASCASCASCAS'),
+
+            RaisedButton(
+              onPressed: () {
+              Navigator.push(context,MaterialPageRoute(builder: (context) => ProfileScreen()));  
+              },
+              
+              child: Text('Fazer login'),
+            ),
+            Container(   
+              padding: const EdgeInsets.only(top: 0.0, left:30,),
+              alignment: Alignment.topLeft, 
+              color: Color(0xFF1A8474),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+              ),
+            ),            
+          ],
+        ),
+      ),
       
+        ],
+      ),
     );
   }
 }
