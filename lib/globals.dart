16b6e352b44d4c6f19dc5f23f60c4ed7e25109a7
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-10-25 22:38:17 -0300

    Adiciona user no global

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,3 +1,7 @@
 import 'package:saude_no_bolso/models/patient.dart';
 
+import 'models/user.dart';
+
 Patient patient = Patient('', '', '', '', '', '', '', '', '','');
+
+UserAccount user = UserAccount('', '', '');
