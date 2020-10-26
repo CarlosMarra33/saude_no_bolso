9a0599777f214876e55abed7efa28f2050c348cc
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-10-25 22:37:15 -0300

    Adiciona o atributo photo no user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,9 +3,11 @@ import 'package:saude_no_bolso/models/patient.dart';
 class UserAccount {
   String email;
   String accountType;
+  String photo;
 
-  UserAccount(String email, String accountType) {
+  UserAccount(String email, String accountType, String photo) {
     this.email = email;
     this.accountType = accountType;
+    this.photo = photo;
   }
 }
